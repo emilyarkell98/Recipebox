c219eb1060b488b2c2857adc690ec78922828d5e
Emily Jane Arkell <emilyarkell@Emilys-MacBook-Pro.local>
2024-05-27 14:00:17 +0100

    xMark for close and better spacing of saved recipes

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { BookmarkProvider } from './context/BookmarkContext';
 import RecipeList from './components/RecipeList';
 import BookmarkModal from './components/BookmarkModal';
-import { BookmarkIcon as BookmarkIconSolid } from '@heroicons/react/24/solid';
 import './styles/Styles.css';
 import Header from './components/Header';
 
--- src/components/BookmarkModal.tsx
+++ src/components/BookmarkModal.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useBookmarks } from '../context/BookmarkContext';
-import { XMarkIcon } from '@heroicons/react/24/outline';
+import { XMarkIcon } from '@heroicons/react/24/solid';
 
 const BookmarkModal: React.FC<{ onClose: () => void }> = ({ onClose }) => {
     const { bookmarks, removeBookmark } = useBookmarks();
@@ -9,17 +9,17 @@ const BookmarkModal: React.FC<{ onClose: () => void }> = ({ onClose }) => {
         <div className="bookmark-modal">
             <div className="bookmark-modal-content">
                 <div className="bookmark-modal-header">
-                    <h2>Saved Recipes</h2>
-                    <button className="bookmark-modal-close-icon" onClick={onClose}>
-                        <XMarkIcon className="bookmark-modal-close-icon" />
+                    <h2 className="bookmark-modal-title">Saved Recipes</h2>
+                    <button className="bookmark-modal-close" onClick={onClose}>
+                        <XMarkIcon className='book-modal-close-xmark'/>
                     </button>
                 </div>
                 <div className="bookmark-modal-body">
                     {bookmarks.map((recipe) => (
-                        <div className="bookmark-item">
+                        <div className="bookmark-item" key={recipe.id}>
                             <img src={recipe.image[0].url} alt={recipe.title} className="bookmark-image" />
                             <div className="bookmark-details">
-                                <h3 className="bookmark-title">{recipe.title}</h3>
+                                <h3 className="bookmark-item-title">{recipe.title}</h3>
                             </div>
                             <button onClick={() => removeBookmark(recipe.id)}>Remove</button>
                         </div>
